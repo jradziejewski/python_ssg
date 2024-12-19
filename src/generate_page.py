6bc71e38ebde99a100d8c67002e627bc743b7cea
jradziejewski <192462@student.ue.wroc.pl>
2024-12-19 22:52:51 +0100

    add recursive page generator

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,4 +1,4 @@
-from copy_static import copy_static
+import os
 from extract_title import extract_title
 from markdown_to_html_node import markdown_to_html_node
 
@@ -16,3 +16,15 @@ def generate_page(from_path, template_path, destin_path):
 
     with open(destin_path, "w") as index_html:
         index_html.write(full_html)
+
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+    contents = os.listdir(dir_path_content)
+
+    for content in contents:
+        path_to_content = os.path.join(dir_path_content, content)
+        if os.path.isfile(path_to_content):
+            generate_page(path_to_content, template_path, os.path.join(dest_dir_path, "index.html"))
+            
+        elif os.path.isdir(path_to_content):
+            os.mkdir(os.path.join(dest_dir_path, content))
+            generate_page_recursive(path_to_content, template_path, os.path.join(dest_dir_path, content))
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import os
 from copy_static import copy_static
-from generate_page import generate_page
+from generate_page import generate_page_recursive
 
 def main():
-    print("Copying static files to public")
+    print("Copying static files to public...")
     copy_static()
 
-    print("Generating page")
-    generate_page("content/index.md", "template.html", os.path.join("./public", "index.html"))
+    print("Generating page...")
+    generate_page_recursive("content", "template.html", os.path.join("./public"))
 
+    print("Serving page...")
 main()
